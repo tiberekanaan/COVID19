bbc992c8c5ff090a18e2e6b72676028fcb9e29e3
Kanaan Ngutu <destinykaka@gmail.com>
2020-09-01 11:41:46 +1200

    Drastic changes
    Replaced older drop down header with default theme templates header.
    Footer inserted.  Repatriation section saw some changes in text.

---
+++ docroot/themes/custom/covid/js/script.js
@@ -0,0 +1,15 @@
+/**
+ * @file
+ * Behaviors for the covid theme.
+ */
+
+(function ($, _, Drupal, drupalSettings) {
+  'use strict';
+
+  Drupal.behaviors.covid = {
+    attach: function (context) {
+      // Vartheme subtheme JavaScript behaviors goes here.
+      }
+  };
+
+})(window.jQuery, window._, window.Drupal, window.drupalSettings);
